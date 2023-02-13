e3bbd975ef6b0e3d09f59115d09084ab8941b854
prashanth24 <prashanth.1si19sfc02@gmail.com>
2023-02-13 19:29:58 +0530

    Second change

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -13,17 +13,17 @@ public class Main {
     public static class B extends A{
 
         public String meth(){
-            return "Method is overridden in Extended class B";
+            return "Method is overridden in Extendend class B";
         }
 
     }
 
     public static void main(String[] args) {
-        B obj1 = new B();
+        A obj1 = new A();
         obj1.meth();
 
         B obj2 = new B();
-        obj2.meth();
+        System.out.println(obj2.meth());
 
 
 
